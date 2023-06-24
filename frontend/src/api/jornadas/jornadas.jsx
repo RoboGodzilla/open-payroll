ba27264593fe8fbaa4ad6e94c4023843edb44335
JostinMoreno <73071911+JostinMoreno@users.noreply.github.com>
2023-06-24 17:02:18 -0600

    Add files via upload

--- frontend/src/api/jornadas/jornadas.jsx
+++ frontend/src/api/jornadas/jornadas.jsx
@@ -3,6 +3,8 @@ import axios from "../config";
 export const getJornadas = () => axios.get("jornada/");
 export const getJornadaById = (id) => axios.get(`jornada/${id}`);
 
+export const addJornada = (payload) => axios.post(`jornada/`, payload);
+
 export const updateJornada = (payload) => {
   // console.log(payload);
   axios.put(`jornada/${payload.id}/`, payload);
