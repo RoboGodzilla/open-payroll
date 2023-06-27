e02cdd73504474deb2cb5df42acb1e2f9cc3c61a
Roberto Zepeda <robertojesezepedacalero@gmail.com>
2023-06-27 01:01:08 -0600

    Nomina Planilla serializer data update

--- backend/nomina/serializers.py
+++ backend/nomina/serializers.py
@@ -9,11 +9,13 @@ class JornadaSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class NominaSerializer(serializers.ModelSerializer):
+    empleadoinfo = EmpleadoSerializer(source='empleado', read_only=True)
     class Meta:
         model = Nomina
         fields = '__all__'
 
 class PlanillaSerializer(serializers.ModelSerializer):
+    nominainfo = NominaSerializer(source='nominas', read_only=True, many=True)
     class Meta:
         model = Planilla
         fields = '__all__'
